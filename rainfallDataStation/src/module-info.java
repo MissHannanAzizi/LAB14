faf9bbcf87e651549b9e13c8cd6c3ec2a6213c61
Miss Hannan Azizi <hannanazizi@gmail.com>
2023-06-10 00:29:47 +0800

    The Exercise 5

---
+++ rainfallDataStation/src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * @author hanna
+ *
+ */
+module rainfallDataStation {
+	
+	
+}
---
+++ rainfallDataStation/src/rainfallDataStation/RainfallDataWriter.java
@@ -0,0 +1,45 @@
+package rainfallDataStation;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+
+
+public class RainfallDataWriter {
+    private String dataFilePath;
+    private List<RainfallData> rainfallDataList;
+
+    public RainfallDataWriter(String dataFilePath) {
+        this.dataFilePath = dataFilePath;
+        this.rainfallDataList = new ArrayList<>();
+    }
+
+    public void addRainfallData(RainfallData rainfallData) {
+        rainfallDataList.add(rainfallData);
+    }
+
+    public void writeDataToFile() {
+    	 try (BufferedWriter writer = new BufferedWriter(new FileWriter(dataFilePath))) {
+             for (RainfallData rainfallData : rainfallDataList) {
+                 // Create a line with the data fields separated by commas
+                 String line = rainfallData.getStationId() + "," +
+                         rainfallData.getStation() + "," +
+                         rainfallData.getDistrict() + "," +
+                         rainfallData.getLastUpdated() + "," +
+                         rainfallData.getDailyRainfall() + "," +
+                         rainfallData.getRainfallFromMidnight() + "," +
+                         rainfallData.getTotalOneHour();
+
+                 // Write the line to the file
+                 writer.write(line);
+                 writer.newLine();
+             }
+            System.out.println("Data creation completed. Data written to file: " + dataFilePath);
+        } catch (IOException e) {
+            System.out.println("Error writing data to file: " + e.getMessage());
+        }
+    }
+}
