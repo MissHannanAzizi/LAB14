d511517c094fec801e1d909df118fa7b5dcf5f69
Miss Hannan Azizi <hannanazizi@gmail.com>
2023-06-24 13:54:51 +0800

    server-side solution

---
+++ TCPSummationServerApp/src/controller/NumberCalculator.java
@@ -0,0 +1,36 @@
+package controller;
+
+
+/**
+ * This is a controller class that manage the arithmetic operations.
+ * 
+ * @author nurulhannan
+ *
+ */
+
+public class NumberCalculator {
+	
+	
+	/**
+	 * A method that will get the summation from three numbers
+	 * @param number1
+	 * @param number2
+	 * @param number3
+	 * @return
+	 */
+	
+	//SUM METHOD
+	public int getSum(int number1, int number2, int number3) {
+		
+		return number1 + number2 + number3;
+		
+	}
+	
+	//MULTIPLICATION METHOD
+	public int getMultiplication(int number1, int number2, int number3) {
+		
+		return number1 * number2 * number3;
+		
+	}
+
+}
